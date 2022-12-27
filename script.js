4fa4295daac08bece2318a83798f90e796102d73
Jordan Toledo <Jordan-direito12@hotmail.com>
2022-12-27 03:49:12 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// cofigurando dark mod
+
+const body =  document.querySelector("body"),
+    
+    sidebar = body.querySelector(".sidebar"),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwtich = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+
+    toggle.addEventListener('click', () =>{
+        sidebar.classList.toggle("close");
+    })
+
+    modeSwtich.addEventListener('click', () =>{
+        body.classList.toggle("dark");
+
+        if(body.classList.contains("dark")){
+            modeText.innerHTML = "Noite"
+        }else{
+            modeText.innerHTML = "Dia"
+        }
+    })
